4d77f7cac857ddc5328f023fe092d713e07c2160
Alejandro Trejo S <Alejandro Trejo S>
2016-11-15 10:14:44 -0500

    15 NOV Removio el video

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+    
+?>
+<!doctype html>
+<html lang="es">
+    <head>
+    <!-- Metadatos -->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Directorio</title>
+        <meta name="author" content="Reef Studios">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">        
+        <link rel='shortcut icon' type='image/x-icon' href='favicon.ico'/>
+        <!-- Hojas de estilos -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/generics.css">
+        <link rel="stylesheet" href="fonts/flaticon.css">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,700,900" rel="stylesheet">
+        <link href="animate.css" rel="stylesheet">
+    <!-- Fin Metadatos -->
+    </head>
+    <body>
+    <!-- Navegadores obsoletos -->
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">Estás usando una versión <strong>obsoleta</strong> del navegador. Porfavor <a href="http://browsehappy.com/">Actualiza tu navegador para poder disfrutar de todo el contenido del directorio.</p>
+        <![endif]-->
+    <!-- Fin Navegadores obsoletos -->
+    
+    <!-- Header > DivHeader > row > Nav > ul > li > div -->
+    <div class="mainScreen">
+        <header>
+            <div class="DivHeader">
+                <div class="row">
+                    <nav>
+                        <ul>
+                            <li>
+                                <div class="col-xs-4 col-md-4 text-center"><a href="">Inicia sesión</a></div>
+                            </li>
+                            <li>
+                                <div class="col-xs-4 col-md-4 text-center"><a href="">¿Quiénes somos?</a></div>
+                            </li>
+                            <li>
+                                <div class="col-xs-4 col-md-4 text-center"><a href="">Contacto</a></div>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>	    						
+            </div>
+        </header>
+  <!-- END Header > DivHeader > row > Nav > ul > li > div -->
+       <div class="content">
+            <div class="weather">
+                <h3><i class="flaticon-cloud"></i> 22 °C</h3>
+            </div>
+            <div class="MainText">
+                <h1 class="animated flipInX">CANCÚN</h1>
+                <h2 class="animated slideInUp">Miles de experiencias, una única locación</h2>
+            </div>
+            <div class="search">
+                    <input id="MainBrowser" type="text" class="form-control animated slideInUp" id="search" placeholder="¿Qué estás buscando?">
+                    <button type="submit" class="btn btn-default animated slideInUp">Buscar</button>
+            </div>
+        </div>
+    </div>
+    
+    <!-- END mainScreen -->
+    
+    
+   <div class="features">
+       <div class="featureHead">
+           <div class="row">
+               <h2>Solo lo mejor</h2>
+               <h3>Tenemos lo mejor para ti</h3>
+           </div>
+           <div class="featurecontent">
+               <div class="content">
+                   
+               </div>
+           </div>
+       </div>
+   </div>
+    <!-- Scripts -->
+        <script src="js/jquery-2.2.4.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/jquery-2.2.4.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    <!-- Fin Scripts -->
+    </body>
+</html>
